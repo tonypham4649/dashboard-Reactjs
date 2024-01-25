a185570ebbf737cbcf810f1a0914bd235fd270d0
tonypham4649 <tonypham2vn@gmail.com>
2024-01-25 15:54:42 +0700

    add team

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Topbar from "./scenes/global/Topbar"
 import Sidebar from "./scenes/global/Sidebar"
 
 import Dashboard from "./scenes/dashboard"
-// import Team from "./scenes/team"
+import Team from "./scenes/team"
 // import Invoices from "./scenes/invoices"
 // import Contacts from "./scenes/contacts"
 // import Bar from "./scenes/bar"
@@ -29,8 +29,8 @@ function App() {
           <main className="content">
             <Topbar />
             <Routes>
-              {/* <Route path="/" element={<Dashboard />} /> */}
-              {/* <Route path="/team" element={<Team />} /> */}
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
               {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
